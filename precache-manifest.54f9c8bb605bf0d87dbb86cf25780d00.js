ec9af2786c36f873e1af161ed1d14ad9d2b2b2f8
Crow <devcrowmaster@gmail.com>
2020-05-19 00:28:23 -0500

    Updates

---
+++ precache-manifest.54f9c8bb605bf0d87dbb86cf25780d00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea46d5bc048c6debb245fbe32e97805",
+    "url": "/admin-pets/index.html"
+  },
+  {
+    "revision": "c9fbdb47547c1dd011fd",
+    "url": "/admin-pets/static/css/main.0be68a11.chunk.css"
+  },
+  {
+    "revision": "42bbfe3981cd3c8309ba",
+    "url": "/admin-pets/static/js/2.dc5a777b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/admin-pets/static/js/2.dc5a777b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fbdb47547c1dd011fd",
+    "url": "/admin-pets/static/js/main.2b286287.chunk.js"
+  },
+  {
+    "revision": "4856c55c0971d4efafbe",
+    "url": "/admin-pets/static/js/runtime-main.46900e21.js"
+  }
+]);
